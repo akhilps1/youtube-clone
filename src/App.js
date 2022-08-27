52a2f98f11a8f904d3af7713a2990b75fca3f2b8
akhilps <akhilps595@gmail.com>
2022-08-27 16:37:51 +0530

    file and folder strecture created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetails,
+  ChannelDetails,
+  Search,
+} from "./components";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetails />} />
+          <Route path="/channel/:id" element={<ChannelDetails />} />
+          <Route path="/search/searchTerm" element={<Search />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Feed from "./Feed";
+import VideoDetails from "./VideoDetails";
+import ChannelDetails from "./ChannelDetails";
+import Search from "./SearchFeed";
+
+export { Navbar, Feed, VideoDetails, ChannelDetails, Search };
